4f567427186b9d9dfa0c61e32e6de75679298780
knightspore <cslemon96@gmail.com>
2021-02-21 14:40:13 +0200

    Changed banner size

--- components/ShopBanner.vue
+++ components/ShopBanner.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <div class="text-center bg-cover bg-top" :style="`background-image: url('${require(`~/assets/img/gallery/${shopText.bannerImage}?resize&size=1920*format=webp`)};`">
+  <div class="text-center bg-cover bg-top" :style="`background-image: url('${require(`~/assets/img/gallery/${shopText.bannerImage}?resize&size=1400&format=webp`)};`">
   <div class="py-24 px-8 text-center text-white bg-gray-900 bg-opacity-50">
     <h1 v-html="shopText.bannerTitle" />
     <h2 v-html="shopText.bannerSubtitle" />
